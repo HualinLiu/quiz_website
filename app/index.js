7a945b4153c0dc2302f651d092262e018e5226aa
Ace Baum <jaybaum.03@gmail.com>
2023-11-13 14:44:31 -0500

    added button listeners

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+let ButtonChoice1 = document.getElementById("choice1");
+let ButtonChoice2 = document.getElementById("choice2");
+let ButtonChoice3 = document.getElementById("choice3");
+
+function Choice1Action() {
+    alert("choice 1 clicked");
+}
+ButtonChoice1.addEventListener("click", Choice1Action, false);
+
+function Choice2Action() {
+    alert("choice 2 clicked");
+}
+ButtonChoice2.addEventListener("click", Choice2Action, false);
+
+function Choice3Action() {
+    alert("choice 3 clicked");
+}
+ButtonChoice3.addEventListener("click", Choice3Action, false);
